97205e9c10f636093133325dfe82d505db0e08d5
egeerdogan <ege@erdogan.dev>
2020-11-23 20:00:55 +0300

    23 11

--- 23_11/bubble_sort.py
+++ 23_11/bubble_sort.py
@@ -4,3 +4,31 @@ November 23-24-25, 2020
 
 Bubble sort
 '''
+
+def swap(lst, i, j):
+    temp = lst[i]
+    lst[i] = lst[j]
+    lst[j] = temp
+
+
+def bubble_sort(lst): 
+    length = len(lst)
+    for i in range(length - 1): 
+        for j in range(length - 1):
+            if lst[j] > lst[j+1]:
+                swap(lst, j, j+1)
+    return lst
+
+
+def better_bubble_sort(lst): 
+    length = len(lst)
+    for i in range(length - 1): 
+        for j in range(length - 1 - i):
+            if lst[j] > lst[j+1]:
+                swap(lst, j, j+1)
+    return lst
+
+
+lst = [3, 1, 6, 3, 8, 2, 5, 1, 2, 3, 1, 8, 9]
+print(bubble_sort(lst))
+print(better_bubble_sort(lst))
--- 23_11/strings.py
+++ 23_11/strings.py
@@ -6,3 +6,39 @@ November 23-24-25, 2020
 
 2. Implement a function, remove_whites, which takes a string as input and returns a new string by removing the extra white space characters (“ “, tab character, newline character). In the return string all words should be separated by just a single space character.
 '''
+
+def dna_replica(dna):
+    matches = {
+        'A': 'T',
+        'T': 'A',
+        'C': 'G',
+        'G': 'C' 
+    }
+    result = ''
+    # for char in dna:
+    #     if char == 'A':
+    #         result += 'T'
+    #     elif char == 'T':
+    #         result += 'A'
+    #     elif char == 'C':
+    #         result += 'G'
+    #     else:
+    #         result += 'C'
+    for char in dna:
+        result += matches[char]
+    return result
+
+
+# '\n' -> newline
+# '\t' -> tab
+def remove_whites(text):
+    result = ''
+    for char in text:
+        if char == ' ':
+            if result[-1] != ' ':
+                result += char
+        elif char not in ['\n', '\t']:
+            result += char
+    return result
+
+
